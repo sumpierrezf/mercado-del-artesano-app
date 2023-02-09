46343853938f19b5e9eacf07a0657355551a187a
LuisPiriz <manupirizcorrea@gmail.com>
2023-02-09 20:47:09 +0000

    Agregados los botones funcionales de login, register, carrito y menu con favs y logout al navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,16 +1,95 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
 import logo3 from "../../img/logo3.png";
 
 export const Navbar = () => {
+  const { store, actions } = useContext(Context);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    actions.logout(); //cerrar la sesion
+    navigate("/login"); //usamos navigate para redireccionar
+  }
+  function handleLogin() {
+    navigate("/login"); //usamos navigate para redireccionar
+  }
+  function handleSignup() {
+    navigate("/signup"); //usamos navigate para redireccionar
+  }
+  function handleFavs() {
+    navigate("/favs/1"); //usamos navigate para redireccionar
+  }
+  function handleCart() {
+    navigate("/cart/1"); //usamos navigate para redireccionar
+  }
   return (
     <nav className="bg-naranja-200 border-naranja-400 navbar-light ">
-      <div className="container-fluid">
+      <div className="container-fluid d-flex">
         <Link to="/">
           <span className="navbar-brand mb-0 h1">
             <img className="logo" src={logo3} width="200" height="80" />
           </span>
         </Link>
+        <div className="d-flex w-100 justify-content-end">
+          {store.auth === true ? (
+            <>
+              <div className="float-end dropdown my-auto">
+                <button
+                  className="btn bg-naranja-100 text-marron border-marron dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i class="fa fa-gear"></i>
+                  Opciones
+                </button>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#" onClick={handleFavs}>
+                      Favoritos
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Vender
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="dropdown-item"
+                      href="#"
+                      onClick={handleLogout}
+                    >
+                      Cerrar sesión
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <button
+                className="btn bg-naranja-100 text-marron border-marron mx-2 my-auto border-2"
+                onClick={handleCart}
+              >
+                <i className="fa fa-cart-arrow-down"></i>
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                className="btn bg-naranja-100 text-marron my-auto border-marron me-2 border-2 justify-content-end"
+                onClick={handleLogin}
+              >
+                Iniciar sesión
+              </button>
+              <button
+                className="btn bg-naranja-100 text-marron border-marron my-auto me-2 border-2 justify-content-end"
+                onClick={handleSignup}
+              >
+                Crear usuario
+              </button>
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );
