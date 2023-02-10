3ec5036245ce5d8e19557d0f36e3ebebaa1709e3
CamilaBur <burgueno.camila03@gmail.com>
2023-02-10 20:16:13 +0000

    aun no anda

--- migrations/versions/5652eaca132d_.py
+++ migrations/versions/5652eaca132d_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 891a6decbbfc
+Revision ID: 5652eaca132d
 Revises: 
-Create Date: 2023-02-09 20:58:38.565975
+Create Date: 2023-02-10 15:12:31.543345
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '891a6decbbfc'
+revision = '5652eaca132d'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,11 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import logo3 from "../../img/logo3.png";
 
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [products, setProducts] = useState([]);
+
   const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   fetch(
+  //     "https://3001-sumpierrezf-mercadodela-zm7bvmnma0m.ws-us86.gitpod.io/api/products"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setProducts(data);
+  //     });
+  // }, []);
 
   function handleLogout() {
     actions.logout(); //cerrar la sesion
@@ -26,73 +39,109 @@ export const Navbar = () => {
   function handleForm() {
     navigate("/form"); //usamos navigate para redireccionar
   }
+  // function filteredProducts() {
+  //   fetch(
+  //     "https://3001-sumpierrezf-mercadodela-zm7bvmnma0m.ws-us86.gitpod.io/api/products"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const filteredProducts = data.filter((product) =>
+  //         product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  //       );
+  //       console.log("aqui esta tu producto" + filteredProducts);
+  //     });
+  // }
+  function filteredProducts() {
+    fetch(
+      "https://3001-sumpierrezf-mercadodela-zm7bvmnma0m.ws-us86.gitpod.io/api/products"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const filteredProducts = data.filter((product) =>
+          product.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      });
+    // console.log(setProducts(filteredProducts));
+  }
   return (
     <nav className="bg-naranja-200 border-naranja-400 navbar-light ">
+      <div>
+        {products.map((product, index) => (
+          <p key={index}>{product.name}</p>
+        ))}
+      </div>
       <div className="container-fluid d-flex">
         <Link to="/">
           <span className="navbar-brand mb-0 h1">
             <img className="logo" src={logo3} width="200" height="80" />
           </span>
         </Link>
-        <div className="d-flex w-100 justify-content-end">
-          {store.auth === true ? (
-            <>
-              <div className="float-end dropdown my-auto">
-                <button
-                  className="btn bg-naranja-100 text-marron border-marron dropdown-toggle"
-                  type="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <i class="fa fa-gear"></i>
-                  Opciones
-                </button>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#" onClick={handleFavs}>
-                      Favoritos
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#" onClick={handleForm}>
-                      Vender
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="dropdown-item"
-                      href="#"
-                      onClick={handleLogout}
-                    >
-                      Cerrar sesión
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <button
-                className="btn bg-naranja-100 text-marron border-marron mx-2 my-auto border-2"
-                onClick={handleCart}
-              >
-                <i className="fa fa-cart-arrow-down"></i>
-              </button>
-            </>
-          ) : (
-            <>
-              <button
-                className="btn bg-naranja-100 text-marron my-auto border-marron me-2 border-2 justify-content-end"
-                onClick={handleLogin}
-              >
-                Iniciar sesión
-              </button>
-              <button
-                className="btn bg-naranja-100 text-marron border-marron my-auto me-2 border-2 justify-content-end"
-                onClick={handleSignup}
-              >
-                Crear usuario
-              </button>
-            </>
-          )}
+        <div className="d-flex w-100 justify-center">
+          <form onSubmit={filteredProducts}>
+            <input
+              type="text"
+              id="searchInput"
+              placeholder="Buscar producto"
+              value={searchTerm}
+              onChange={(event) => setSearchTerm(event.target.value)}
+            />
+            <button type="submit">Buscar</button>
+          </form>
         </div>
+      </div>
+      <div className="d-flex w-100 justify-content-end">
+        {store.auth === true ? (
+          <>
+            <div className="float-end dropdown my-auto">
+              <button
+                className="btn bg-naranja-100 text-marron border-marron dropdown-toggle"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Opciones
+              </button>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#" onClick={handleFavs}>
+                    Favoritos
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#" onClick={handleForm}>
+                    Vender
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#" onClick={handleLogout}>
+                    Cerrar sesión
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <button
+              className="btn bg-naranja-100 text-marron border-marron mx-2 my-auto border-2"
+              onClick={handleCart}
+            >
+              <i className="fa fa-cart-arrow-down"></i>
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              className="btn bg-naranja-100 text-marron my-auto border-marron me-2 border-2 justify-content-end"
+              onClick={handleLogin}
+            >
+              Iniciar sesión
+            </button>
+            <button
+              className="btn bg-naranja-100 text-marron border-marron my-auto me-2 border-2 justify-content-end"
+              onClick={handleSignup}
+            >
+              Crear usuario
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
