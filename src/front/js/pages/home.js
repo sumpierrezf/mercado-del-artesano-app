071a05e5cf946ad93258d631c633e67dc398ec35
LuisPiriz <manupirizcorrea@gmail.com>
2023-02-22 15:25:10 +0000

    Fondo elegido

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { Catalogo } from "../component/catalogo";
+import banner1 from "../../img/banner1.jpg";
+
 export const Home = () => {
   const { store, actions } = useContext(Context);
   useEffect(() => {
@@ -14,22 +16,22 @@ export const Home = () => {
       <div
         className="w-100"
         style={{
+          // backgroundImage: `url(${banner1})`,
+          // backgroundImage:
+          //   "url(https://previews.123rf.com/images/malven/malven1711/malven171100034/90439031-fondo-y-textura-de-lana-tejida-o-tela-de-algod%C3%B3n-color-marr%C3%B3n-de-primer-plano.jpg)",
+          // backgroundImage:
+          //   "url(https://static.vecteezy.com/system/resources/previews/005/273/505/non_2x/light-brown-knitted-fabric-texture-background-top-view-copy-empty-space-for-text-photo.jpg)",
           backgroundImage:
-            "url(https://static.vecteezy.com/system/resources/previews/004/727/248/non_2x/realistic-knitted-background-with-snowflakes-seamless-texture-of-wool-gray-knit-norwegian-pattern-template-of-knitwear-for-wallpaper-christmas-and-new-year-greeting-card-webpage-backdrop-vector.jpg)",
+            "url(https://img.freepik.com/fotos-premium/detalle-textura-ratan-artesania-bambu-tejido-textura-fondo-textura-tejido-artesanal_298352-891.jpg?w=2000)",
+          // backgroundImage:
+          //   "url(https://img.freepik.com/fotos-premium/textura-lana-tejido-punto-cerca_110241-247.jpg?w=2000)",
           backgroundSize: "100%",
-          height: "500px",
+          height: "465px",
         }}
       >
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <div className="text-light my-auto text-center h-100 d-flex align-items-center">
+          <h1 className="mx-auto">Mercado del Artesano</h1>
+        </div>
       </div>
       <div className="container-fluid bg-naranja-100">
         <div className="row">
