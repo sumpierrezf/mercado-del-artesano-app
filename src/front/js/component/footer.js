32dbe5111f9879e765003166a6675ff8463b3372
LuisPiriz <manupirizcorrea@gmail.com>
2023-02-23 19:52:22 +0000

    Mejorada un poco la estética de la vista contacto

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
   <footer className="footer bg-naranja-300 shadow-lg mt-auto py-3 text-center text-light w-100 h-auto d-flex">
     <div className="col-6 d-flex align-items-center">
-      <p className="my-auto mx-auto text-shadow">
+      <p className="my-auto mx-auto px-1 text-shadow">
         Hecho con 🧡 por{" "}
         <a className="text-marron" href="http://www.mercadodelartesano.com">
           Mercado del Artesano
